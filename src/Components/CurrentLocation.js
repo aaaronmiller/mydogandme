3a3e87be7a8ef441a01745bcc36fda972ac64a94
Aaron Miller <aaaronmiller@gmail.com>
2021-05-28 23:45:34 -0700

    server

--- src/Components/CurrentLocation.js
+++ src/Components/CurrentLocation.js
@@ -123,8 +123,8 @@ export default CurrentLocation;
 CurrentLocation.defaultProps = {
   zoom: 14,
   initialCenter: {
-    lat: -1.2884,
-    lng: 36.8233
+    lat: 47.6062,
+    lng: -122.3321
   },
   centerAroundCurrentLocation: false,
   visible: true
